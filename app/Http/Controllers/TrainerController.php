626506479a39830c5891f6dcfe9c061e215c491c
haitchex <canti89@gmail.com>
2018-05-20 10:19:22 -0500

    implicit binding

--- app/Http/Controllers/TrainerController.php
+++ app/Http/Controllers/TrainerController.php
@@ -56,9 +56,9 @@ class TrainerController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($slug)
+    public function show(Trainer $trainer)
     {
-        $trainer = Trainer::where('slug', $slug)->firstOrFail();
+        // $trainer = Trainer::where('slug', $slug)->firstOrFail();
         return view('trainers.show', compact('trainer'));
     }
 
