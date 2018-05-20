4047d3ffb72ed5326c9300719f336d9e8dcacf3e
haitchex <canti89@gmail.com>
2018-05-20 09:41:05 -0500

    vista trainers

--- resources/views/trainers/index.blade.php
+++ resources/views/trainers/index.blade.php
@@ -4,12 +4,18 @@
     <div class="row">
     @foreach ($trainers as $trainer)
         <div class="col-sm">
-            <div class="card" style="width: 18rem;">
+            <div class="card" style="width: 18rem; margin-top: 70px">
             {{-- <img class="card-img-top" src="..." alt="Card image cap"> --}}
-                <div class="card-body">
-                    <img class="card-img-top" src="{{Storage::url($trainer->avatar)}}" alt="">
+                <div class="card-body text-center">
+                    <img
+                    style="height: 100px; width: 100px; background-color: #EFEFEF; mx-auto; display-block; margin: 20px"
+                        class="card-img-top rounded-circle"
+                        src="{{Storage::url($trainer->avatar)}}" alt="">
                     <h5 class="card-title">{{$trainer->name}}</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                    </p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
