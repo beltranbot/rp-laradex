cfa1f03ef8ee8136c4bd163a212a343b7052c142
beltranbot@gmail.com <canti89@gmail.com>
2018-05-26 12:42:27 -0500

    25 - subviews

--- resources/views/trainers/edit.blade.php
+++ resources/views/trainers/edit.blade.php
@@ -11,18 +11,7 @@
             'files' => true
         ]
     )!!}
-        <div class="form-group">
-            {!!Form::label('name', 'Nombre') !!}
-            {!!Form::text('name', null, ['class' => 'form-control'])!!}
-        </div>
-        <div class="form-group">
-            {!!Form::label('slug', 'Slug') !!}
-            {!!Form::text('slug', null, ['class' => 'form-control'])!!}
-        </div>
-        <div class="form-group">
-            {!!Form::label('avtar', 'Avatar') !!}
-            {!!Form::file('avatar')!!}
-        </div>
+        @include('trainers.form')
         {!!Form::submit('Actualizar', ['class' => 'btn btn-primary'])!!}
     {!!Form::close()!!}
 @endsection
