3229f2371aa62b556f733f0b2ef14aced213a8c5
carlos.beltran-at-674969126656 <canti89@gmail.com>
2018-09-07 11:04:14 -0500

    34 - user roles  models & migrations

---
+++ app/Role.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace laradex;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    public function users() {
+        return $this->belongsToMany('App\User');
+    }
+}
