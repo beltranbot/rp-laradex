043d157c31eee608b35c52f73c0353c51ae775b6
hx <beltranbot@gmail.com>
2018-10-02 20:38:54 -0500

    35 - roles de usuarios seeders

--- app/User.php
+++ app/User.php
@@ -28,6 +28,6 @@ class User extends Authenticatable
     ];
 
     public function roles () {
-        return $this->belongsToMay('App\Role');
+        return $this->belongsToMany('laradex\Role');
     }
 }
