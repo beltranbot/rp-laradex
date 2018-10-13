285259d7d0b260d9bbb4f76273c57527decf1bf1
hx <beltranbot@gmail.com>
2018-10-13 10:53:05 -0500

    41 - peticion http con axios

--- app/Http/Controllers/PokemonController.php
+++ app/Http/Controllers/PokemonController.php
@@ -6,7 +6,12 @@ use Illuminate\Http\Request;
 
 class PokemonController extends Controller
 {
-    public function index () {
+    public function index (Request $request) {
+        if ($request->ajax()) {
+            return response()->json([
+                ['id' => 1, 'name' => 'Pikachu']
+            ]);
+        }
         return view('pokemon.index');
     }
 }
