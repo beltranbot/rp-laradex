b506b4cbcfaa4551698b74db4d6020ecf5db50c4
hx <beltranbot@gmail.com>
2018-10-13 08:30:31 -0500

    40 - listando recursos con vue

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -16,6 +16,7 @@ window.Vue = require('vue');
  */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('pokemon-component', require('./components/PokemonComponent.vue'));
 
 const app = new Vue({
     el: '#app'
--- resources/views/pokemon/index.blade.php
+++ resources/views/pokemon/index.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.app')
 @section('content')
-    <h1>Hola</h1>
-    <example-component></example-component>
+    <pokemon-component></pokemon-component>
 @endsection
